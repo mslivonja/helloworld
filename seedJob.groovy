af7d45bcb51289a94f77b91d58dde2a2fd0e8c4b
Matija Slivonja <matija.slivonja@ericsson.com>
2019-12-20 12:07:49 +0100

    Added post build action. Added junit test reporting.

--- seedJob.groovy
+++ seedJob.groovy
@@ -2,6 +2,7 @@ def gitUrl = "https://github.com/mslivonja/helloworld.git"
 
 job("HelloWorld-Project-Build") {
     description "Builds My HellWorld project from master branch."
+
     parameters {
         stringParam('COMMIT', 'HEAD', 'Commit to build')
     }
@@ -22,6 +23,16 @@ job("HelloWorld-Project-Build") {
             goals('clean')
             goals('package')
             mavenInstallation('Maven 3.6.3')
+        }
+    }
+    publishers{
+        archiveJunit('**/target/surefire-reports/*.xml'){
+            allowEmptyResults()
+            retainLongStdout()
+            healthScaleFactor(1.5)
+            testDataPublishers{
+                publishTestStabilityData()
+            }
         }
     }
 }
